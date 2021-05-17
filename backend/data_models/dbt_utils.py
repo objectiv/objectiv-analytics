0794ae7f906f44becaefa57aba2dc0caf0000aa2
thijs <thijs@getobjectiv.com>
2021-05-17 23:01:15 +0200

    WIP

--- backend/data_models/dbt_utils.py
+++ backend/data_models/dbt_utils.py
@@ -4,9 +4,13 @@ Copyright 2021 Objectiv B.V.
 from typing import Optional, Dict
 
 from dbt import tracking
+from dbt.adapters.factory import get_adapter
+from dbt.contracts.graph.manifest import Manifest
+from dbt.contracts.graph.parsed import ParsedModelNode
 from dbt.main import parse_args
 from dbt.task.compile import CompileTask
 from dbt.tracking import User
+from networkx import DiGraph
 
 
 def get_compile_task(model: Optional[str], vars: Dict[str, str]) -> CompileTask:
@@ -46,6 +50,63 @@ def get_sql(model: Optional[str], vars: Dict[str, str]) -> str:
     return last_result.node.compiled_sql
 
 
+def get_sql2(model: str, vars: Dict[str, str]) -> str:
+    """
+    Get the sql for a single model.
+
+    WIP
+    """
+    tracking.active_user = User('')
+    tracking.active_user.do_not_track = True
+
+    # todo: get this through magic
+    full_model_name = f'model.objectiv.{model}'
+
+    ctask = get_compile_task(model, vars)
+    # build manifest based on project files
+    ctask.load_manifest()
+    # build graph, does not compile individual nodes
+    ctask.compile_manifest()
+
+    manifest = ctask.manifest
+    node = manifest.nodes[full_model_name]
+    node.config.materialized = 'ephemeral'
+
+    set_previous_nodes_materialization(
+        manifest=manifest,
+        graph=ctask.graph.graph,
+        node=node,
+        depth=2
+    )
+
+    adapter = get_adapter(ctask.config)
+    compiler = adapter.get_compiler()
+    compiled_node = compiler.compile_node(node=node, manifest=manifest, extra_context=None, write=False)
+    return compiled_node.compiled_sql
+
+
+def set_previous_nodes_materialization(manifest: Manifest,
+                                       graph: DiGraph,
+                                       node: ParsedModelNode,
+                                       depth: int):
+    """
+    Set node and the levels depth-1 below that to ephemeral, the level below depth-1 is set to view
+    """
+    if depth == 0:
+        node.config.materialized = 'view'
+        print(node.unique_id, node.config.materialized)
+        return
+    node.config.materialized = 'ephemeral'
+    print(node.unique_id, node.config.materialized)
+    for prev_node_id in graph.pred[node.unique_id]:
+        prev_node = manifest.nodes[prev_node_id]
+        set_previous_nodes_materialization(manifest, graph, prev_node, depth-1)
+
+
+
+
+
+
 """
 # Notes
 
@@ -54,6 +115,7 @@ def get_sql(model: Optional[str], vars: Dict[str, str]) -> str:
 * flags
 * log_manager
 * ui
+* current working directory
 
 
 ## Code flow:
@@ -81,6 +143,23 @@ main:main()
             -> task.run(): The run() implementation depends on the Task sub-type, for compile and run the
                     relevant implementation is dbt.task.runnable.GraphRunnableTask.run
                 -> _runtime_initialize()
+                    -> super()._runtime_initialize()
+                         -> load_manifest()
+                            -> ManifestLoader.get_full_manifest(self.config):
+                                - projects = config.load_dependencies(): load all projects (e.g. other dbt_modules)
+                                - loader = ManifestLoader(config, projects, macro_hook):
+                                    Create a ManifestLoader. This constructor already might do i/o. if 'partial parsing' is enabled as flag or in the project settings
+                                -> loader.load(): This is were all files get read, parsed and the graph constructed (TODO: check?)
+                                    - per project: read all files (models, macros's, test, seed, docs, etc.)
+                                    - parse all macros
+                                    - reparse all macros and resolve dependencies between them
+                                    # The macros need to be parsed before other files are parsed, because
+                                    # other files might depend on them
+                                    -> parse_project(project, files):
+                                        parse all files in project, except for macros those are done already
+                                    
+                            - write_manifest(): write target/manifest.json
+                         - compile_manifest()
                     - `self.job_queue = self.get_graph_queue()`: initialize the initial job-queue. The
                         get_graph_queue() implementation is Task-type dependent
                     - Fill self._flattened_nodes. It seems this contains the exact same nodes as
@@ -100,9 +179,16 @@ main:main()
                     -> after_run(adapter, result)
                     -> self.after_hooks(adapter, result, elapsed): print some stuff
 
-## Nodes
-DBT builds a dependency graph of all models. The nodes are all put into a queue (in the right order). The
-nodes are taken from the queue one by one, and for each a Runner is created that executes the part of
+## Manifest, Graph and Nodes
+The Manifest object dbt.contracts.graph.manifest.Manifest contains the full graph of models that need to
+be run. Each model being a node in the graph. A node is uniquely identified by it's name (e.g.
+'model.objectiv.aggregated_features').
+
+The ManifestLoader builds the manifest based on the files in the project-dir, and all includes projects.
+
+## Task
+The nodes are all put into a queue (in the right order).
+The nodes are taken from the queue one by one, and for each a Runner is created that executes the part of
 the overall Task that pertains to that node.
 See dbt.task.runnable.GraphRunnableTask.run_queue
 
