57510ea525f73b3e257b14c3815ddf553da77f08
mike <mike@objectiv.io>
2021-10-20 16:47:30 +0200

    updated pg-http-gateway-wrapper: - now uses pickle to encode responses - connection management - send result description (columns in result set)
    and lots of other fixes

--- buhtuh/jupyter-lite/app/app.py
+++ buhtuh/jupyter-lite/app/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, make_response, request
+from flask import Flask, Response, make_response, request
 from flask_cors import CORS
 
-import psycopg2, psycopg2.extras as extras
+import psycopg2
+import psycopg2.extras as extras
+
 import os
-import json 
-import datetime
-import decimal
+import json
 from typing import Dict
+import uuid
+import time
+
+import pickle
+import base64
+
 
 _PG_HOSTNAME = os.environ.get('POSTGRES_HOSTNAME', 'localhost')
 _PG_PORT = os.environ.get('POSTGRES_PORT', '5432')
@@ -16,39 +22,30 @@ _PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD', '')
 
 dsn = f"dbname='{_PG_DATABASE_NAME}' user='{_PG_USER}' host='{_PG_HOSTNAME}' password='{_PG_PASSWORD}' port='{_PG_PORT}'"
 
-try:
-    conn = psycopg2.connect(dsn, cursor_factory=extras.DictCursor)
-except Exception as e:
-    print(f'db is b0rken {e}')
-    exit(1)
-cur = conn.cursor()
+# keep track of open connections
+connections: Dict = {}
 
-class MyEncoder(json.JSONEncoder):
 
-    def default(self, obj):
-        if isinstance(obj, (datetime.datetime, datetime.date, datetime.time)):
-            return obj.isoformat()
-        elif isinstance(obj, datetime.timedelta):
-            return (datetime.datetime.min + obj).time().isoformat()
-        elif isinstance(obj, decimal.Decimal):
-            return float(obj)
-        return super(MyEncoder, self).default(obj)
+def serialize(data: Dict) -> str:
+    serialized = pickle.dumps(data)
+    encoded_bytes = base64.b64encode(serialized)
+    return str(encoded_bytes, "utf-8")
 
-def json_encode(data: Dict) -> str:
-    encoder = MyEncoder()
-    return encoder.encode(data)
+
+def unserialize(data: str) -> Dict:
+    return json.loads(data)
+
 
 app = Flask(__name__)
 
-
 CORS(app,
-    resources={r'/*': {
-        'origins': '*',
-        'supports_credentials': True,  # needed for cookies
-        # Setting max_age to a higher values is probably useless, as most browsers cap this time.
-        # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
-        'max_age': 3600 * 24
-        }})
+     resources={r'/*': {
+         'origins': '*',
+         'supports_credentials': True,  # needed for cookies
+         # Setting max_age to a higher values is probably useless, as most browsers cap this time.
+         # See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
+         'max_age': 3600 * 24
+     }})
 
 
 @app.route("/")
@@ -59,24 +56,95 @@ def index():
 
     return "hello"
 
-@app.route("/query", methods=["post"])
-def query():
+
+@app.route("/connect", methods=["post"])
+def db_connect() -> Response:
+    result = {
+        'connection_id': 0,
+        'connected': False
+    }
+    try:
+        conn = psycopg2.connect(dsn, cursor_factory=extras.DictCursor)
+        connection_id = uuid.uuid4().urn
+
+        connections[connection_id] = {
+            "connection": conn,
+            "time": time.time()
+        }
+
+        print(f'Created connection: {connection_id}')
+
+        result['connection_id'] = connection_id
+        result['connected'] = True
+
+    except Exception as e:
+        print(f'db is b0rken {e}')
+
+    return make_response(serialize(result), 200, {"Content-Type": "application/json"})
+
+
+@app.route("/disconnect", methods=["post"])
+def db_disconnect() -> Response:
     raw_data = request.data
-    
-    data = json.loads(raw_data)
+
+    data = unserialize(raw_data)
+    connection_id = data['connection_id']
+
+    try:
+        _get_connection(connection_id).close()
+    except Exception as e:
+        print(f'couldnt find connection {connection_id} for disconnect {e}')
+
+    if connection_id in connections:
+        del connections[connection_id]
+
+    print(f'Closed connection: {connection_id}')
+    result = {'result': 'ok'}
+    return make_response(serialize(result), 200, {"Content-Type": "application/json"})
+
+
+def _get_connection(connection_id: str) :
+    if connection_id in connections:
+        print(f"returning connection {connection_id} -> {connections[connection_id]}")
+        return connections[connection_id]['connection']
+    raise Exception('Could not find connection')
+
+
+@app.route("/query", methods=["post"])
+def db_query() -> Response:
+    raw_data = request.data
+    print(f'trying to unserialize {raw_data}')
+    data = unserialize(raw_data)
+
+    if 'query' not in data:
+        raise Exception('Not enough parameters')
+
+    if 'connection_id' not in data:
+        raise Exception('No connection specified')
+
     query = data['query']
-    
+
+    # run query an get resulting rows
+    cur = _get_connection(connection_id=data['connection_id']).cursor()
     cur.execute(query)
     rows = cur.fetchall()
-    
-    result = []
+
+    # get description of result table columns
+    column_fields = ('name', 'type_code', 'display_size', 'internal_size', 'precision', 'scale', 'null_ok', 'table_oid', 'table_column')
+    description = []
+    for column in cur.description:
+        description.append([getattr(column, f) for f in column_fields if hasattr(column, f)])
+
+    result = {
+        'description': description,
+        'rows': []
+    }
     for row in rows:
-        result.append({k:v for k,v in row.items()})
-    
+        result['rows'].append({k: v for k, v in row.items()})
 
     print(f'got query: {query}')
-    response = json_encode(result)
-    print(f'sending result: {response}')
+    print(result)
+    response = serialize(result)
 
     return make_response(response, 200, {"Content-type": "application/json"})
 
