844e412b6e3451cd625e777dc029266725bf4bd7
Tom Jansen <tom@getobjectiv.com>
2021-10-25 21:42:59 +0200

    Split tests

--- buhtuh/tests/unit/sql_models/test_expression.py
+++ buhtuh/tests/unit/sql_models/test_expression.py
@@ -5,9 +5,6 @@ import pytest
 
 from sql_models.expression import RawToken, ColumnReferenceToken, expression_to_sql, quote_string, \
     quote_identifier, StringValueToken, Expression
-from buhtuh.expression import Expression as BTExpression
-
-from tests.unit.buhtuh.util import get_fake_df
 
 
 def test_construct():
@@ -34,18 +31,6 @@ def test_construct():
         Expression.construct('{}', expr, expr)
 
 
-def test_construct_series():
-    df = get_fake_df(['i'], ['a', 'b'])
-    result = BTExpression.construct('cast({} as text)', df.a)
-    assert result == BTExpression([
-        RawToken('cast('),
-        ColumnReferenceToken('a'),
-        RawToken(' as text)')
-    ])
-    assert expression_to_sql(result.resolve_column_references()) == 'cast("a" as text)'
-
-    result = BTExpression.construct('{}, {}, {}', df.a, BTExpression.raw('test'), df.b)
-    assert expression_to_sql(result.resolve_column_references()) == '"a", test, "b"'
 
 
 def test_column_reference():
@@ -64,18 +49,6 @@ def test_string():
     expr = Expression.string_value('a string \' with quotes\'\' in it')
     assert expr == Expression([StringValueToken('a string \' with quotes\'\' in it')])
     assert expression_to_sql(expr.resolve_column_references()) == "'a string '' with quotes'''' in it'"
-
-
-def test_combined():
-    df = get_fake_df(['i'], ['duration', 'irrelevant'])
-    expr1 = Expression.column_reference('year')
-    expr2 = BTExpression.construct('cast({} as bigint)', df.duration)
-    expr_sum = Expression.construct('{} + {}', expr1, expr2)
-    expr_str = Expression.construct('"Finished in " || cast(({}) as text) || " or later."', expr_sum)
-    assert expression_to_sql(expr_str.resolve_column_references()) == \
-           '"Finished in " || cast(("year" + cast("duration" as bigint)) as text) || " or later."'
-    assert expression_to_sql(expr_str.resolve_column_references('table_name')) == \
-           '"Finished in " || cast(("table_name"."year" + cast("table_name"."duration" as bigint)) as text) || " or later."'
 
 
 def test_quote_string():
