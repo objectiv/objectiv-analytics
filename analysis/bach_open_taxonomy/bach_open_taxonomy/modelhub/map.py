2c3a1f7a300d5b71f715e81aff5fece4a29b721e
hendrik-obj <hendrik@objectiv.ai>
2022-03-11 16:44:21 +0100

    change and add conversion modelas

--- analysis/bach_open_taxonomy/bach_open_taxonomy/modelhub/map.py
+++ analysis/bach_open_taxonomy/bach_open_taxonomy/modelhub/map.py
@@ -83,7 +83,35 @@ class Map:
             series = ((conversion_stack.notnull()) & (df.event_type == conversion_event))
         return series.copy_override(name='is_conversion_event')
 
-    def conversion_count(self, df, name: str, partition='session_id'):
+    def conversions_counter(self, df, name: str, partition='session_id'):
+        """
+        Counts the total number of conversions given a partition (ie session_id
+        or user_id).
+
+        :param name: the name of the conversion to label as set in
+            :py:attr:`ObjectivFrame.conversion_events`.
+        :param partition: the partition over which the number of conversions are counted. Can be any column
+            of the ObjectivFrame
+        :returns: SeriesBoolean with same index as the ObjectivFrame this method is applied to.
+        """
+
+        self._mh._check_data_is_objectiv_data(df)
+
+        df['__conversions'] = self._mh.map.conversions_in_time(df, name=name)
+
+        window = df.groupby(partition).window(end_boundary=WindowFrameBoundary.FOLLOWING)
+        converted = window['__conversions'].max()
+        df = df.drop(columns=['__conversions'])
+
+        new_series = converted.copy_override(name='converted',
+                                             index=df.index).to_frame().materialize().converted
+
+        return new_series
+
+    def conversion_count(self, name: str, partition='session_id'):
+        raise NotImplementedError('function is depecrecated please use `conversions_in_time`')
+
+    def conversions_in_time(self, df, name: str, partition='session_id'):
         """
         Counts the number of time a user is converted at a moment in time given a partition (ie session_id
         or user_id).
@@ -111,11 +139,11 @@ class Map:
                                           df[partition],
                                           df[partition],
                                           df['moment'])
-        df['conversion_count'] = df['__conversion_counter']\
+        df['conversions_in_time'] = df['__conversion_counter']\
             .copy_override_dtype('int64')\
             .copy_override(expression=expression)
 
-        return df.conversion_count
+        return df.conversions_in_time
 
     def pre_conversion_hit_number(self,
                                   df,
