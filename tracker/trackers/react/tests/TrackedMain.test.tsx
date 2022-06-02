42aab3d3c14a3026c5db9594e869aa452f317027
sdirosa <surai@objectiv.io>
2022-06-02 14:12:05 +0200

    Header, Main and Nav

--- tracker/trackers/react/tests/TrackedMain.test.tsx
+++ tracker/trackers/react/tests/TrackedMain.test.tsx
@@ -6,7 +6,14 @@ import { MockConsoleImplementation, SpyTransport } from '@objectiv/testing-tools
 import { LocationContextName } from '@objectiv/tracker-core';
 import { fireEvent, getByText, render } from '@testing-library/react';
 import React from 'react';
-import { ObjectivProvider, ReactTracker, TrackedMain, usePressEventTracker } from '../src';
+import {
+  ObjectivProvider,
+  ReactTracker,
+  TrackedDiv,
+  TrackedMain,
+  TrackedRootLocationContext,
+  usePressEventTracker
+} from '../src';
 
 require('@objectiv/developer-tools');
 globalThis.objectiv?.TrackerConsole.setImplementation(MockConsoleImplementation);
@@ -56,6 +63,77 @@ describe('TrackedMain', () => {
     );
   });
 
+  it('should allow disabling id normalization', () => {
+    const spyTransport = new SpyTransport();
+    jest.spyOn(spyTransport, 'handle');
+    const tracker = new ReactTracker({ applicationId: 'app-id', transport: spyTransport });
+
+    const TrackedButton = ({children}:{children: React.ReactNode}) => {
+      const trackPressEvent = usePressEventTracker();
+      return <div onClick={trackPressEvent}>{children}</div>;
+    };
+
+    const { container } = render(
+      <ObjectivProvider tracker={tracker}>
+        <TrackedMain id={'Main 1'}>
+          <TrackedButton>Trigger Event 1</TrackedButton>
+        </TrackedMain>
+        <TrackedMain id={'Main 2'} normalizeId={false}>
+          <TrackedButton>Trigger Event 2</TrackedButton>
+        </TrackedMain>
+      </ObjectivProvider>
+    );
+
+    jest.resetAllMocks();
+
+    fireEvent.click(getByText(container, /trigger event 1/i));
+    fireEvent.click(getByText(container, /trigger event 2/i));
+
+    expect(spyTransport.handle).toHaveBeenCalledTimes(2);
+    expect(spyTransport.handle).toHaveBeenNthCalledWith(1,
+      expect.objectContaining({
+        _type: 'PressEvent',
+        location_stack: expect.arrayContaining([
+          expect.objectContaining({
+            _type: LocationContextName.ContentContext,
+            id: 'main-1',
+          }),
+        ]),
+      })
+    );
+    expect(spyTransport.handle).toHaveBeenNthCalledWith(2,
+      expect.objectContaining({
+        _type: 'PressEvent',
+        location_stack: expect.arrayContaining([
+          expect.objectContaining({
+            _type: LocationContextName.ContentContext,
+            id: 'Main 2',
+          }),
+        ]),
+      })
+    );
+  });
+
+  it('should console.error if an id cannot be automatically generated', () => {
+        jest.spyOn(console, 'error').mockImplementation(() => {});
+    const tracker = new ReactTracker({ applicationId: 'app-id', transport: new SpyTransport() });
+
+    render(
+      <ObjectivProvider tracker={tracker}>
+        <TrackedRootLocationContext Component={'div'} id={'root'}>
+          <TrackedDiv id={'content'}>
+            <TrackedMain id={'☹️'}/>
+          </TrackedDiv>
+        </TrackedRootLocationContext>
+      </ObjectivProvider>
+    );
+
+    expect(MockConsoleImplementation.error).toHaveBeenCalledTimes(1);
+    expect(MockConsoleImplementation.error).toHaveBeenCalledWith(
+      '｢objectiv｣ Could not generate a valid id for ContentContext @ RootLocation:root / Content:content. Please provide the `id` property.'
+    );
+  })    
+  
   it('should allow specifying a custom id', () => {
     const spyTransport = new SpyTransport();
     jest.spyOn(spyTransport, 'handle');
