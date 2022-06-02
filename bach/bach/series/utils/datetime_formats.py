eca2cd803bbf0b0671c11a79d12d73b18e20b1a0
Kathia Barahona <katbara03@gmail.com>
2022-06-02 11:25:03 +0200

    strftime, deprecate to_sql_format, simplified c codes, parse format only for pg, raise user warnings

--- bach/bach/series/utils/datetime_formats.py
+++ bach/bach/series/utils/datetime_formats.py
@@ -1,213 +1,166 @@
 import re
-from typing import Dict
-
-from sqlalchemy.engine import Engine
-
-from sql_models.util import is_bigquery, is_postgres
+import warnings
 
 # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
-_STANDARD_DATE_FORMAT_CODES = {
+_SUPPORTED_C_STANDARD_CODES = {
     # week codes
-    'WEEKDAY_ABBREVIATED': '%a',
-    'WEEKDAY_FULL_NAME': '%A',
-    'WEEKDAY_NUMBER': '%w',  # Sunday is 0, Saturday is 6
-    'WEEK_NUMBER_OF_YEAR_SUNDAY_FIRST': '%U',
-    'WEEK_NUMBER_OF_YEAR_MONDAY_FIRST': '%W',
+    '%a',  # WEEKDAY_ABBREVIATED
+    '%A',  # WEEKDAY_FULL_NAME
+    '%w',  # WEEKDAY_NUMBER
+    '%U',  # WEEK_NUMBER_OF_YEAR_SUNDAY_FIRST
+    '%W',  # WEEK_NUMBER_OF_YEAR_MONDAY_FIRST
 
     # day codes
-    'DAY_OF_MONTH': '%d',
-    'DAY_OF_MONTH_SUPPRESSED': '%-d',
-    'DAY_OF_MONTH_PRECEDED_BY_A_SPACE': '%e',
-    'DAY_OF_YEAR': '%j',
-    'DAY_OF_YEAR_SUPPRESSED': '%-j',
+    '%d',  # DAY_OF_MONTH
+    '%e',  # DAY_OF_MONTH_PRECEDED_BY_A_SPACE
+    '%j',  # DAY_OF_YEAR
 
     # month codes
-    'MONTH_ABBREVIATED': '%b',
-    'MONTH_ABBREVIATED_2': '%h',
-    'MONTH_FULL_NAME': '%B',
-    'MONTH_NUMBER': '%m',
-    'MONTH_NUMBER_SUPPRESSED': '%-m',
+    '%b',  # MONTH_ABBREVIATED
+    '%h',  # MONTH_ABBREVIATED_2
+    '%B',  # MONTH_FULL_NAME
+    '%m',  # MONTH_NUMBER
 
     # year codes
-    'YEAR_WITHOUT_CENTURY': '%y',
-    'YEAR_WITH_CENTURY': '%Y',
-    'CENTURY': '%C',
-    'QUARTER': '%Q',
-    'MONTH_DAY_YEAR': '%D',  # MM/DD/YY
-    'YEAR_MONTH_DAY': '%F',  # YYYY/MM/DD
+    '%y',  # YEAR_WITHOUT_CENTURY
+    '%Y',  # YEAR_WITH_CENTURY
+    '%C',  # CENTURY
+    '%Q',  # QUARTER
+    '%D',  # MONTH_DAY_YEAR
+    '%F',  # YEAR_MONTH_DAY
 
     # iso 8601 codes
-    'ISO_8601_YEAR_WITH_CENTURY': '%G',
-    'ISO_8601_YEAR_WITHOUT_CENTURY': '%g',
-    'ISO_8601_WEEK_NUMBER_OF_YEAR': '%V',
-    'ISO_8601_WEEKDAY_NUMBER': '%u',  # Monday is 1, Sunday is 7
+    '%G',  # ISO_8601_YEAR_WITH_CENTURY
+    '%g',  # ISO_8601_YEAR_WITHOUT_CENTURY
+    '%V',  # ISO_8601_WEEK_NUMBER_OF_YEAR
+    '%u',  # ISO_8601_WEEKDAY_NUMBER
 
     # time unit codes
-    'HOUR24': '%H',
-    'HOUR24_SUPPRESSED': '%-H',
-    'HOUR12': '%I',
-    'HOUR12_SUPPRESSED': '%-I',
-    'HOUR24_PRECEDED_BY_A_SPACE': '%k',
-    'HOUR12_PRECEDED_BY_A_SPACE': '%l',
-    'MINUTE': '%M',
-    'MINUTE_SUPPRESSED': '%-M',
-    'SECOND': '%S',
-    'SECOND_SUPPRESSED': '%-S',
-    'EPOCH': '%s',
-    'MICROSECOND': '%f',
+    '%H',  # HOUR24
+    '%I',  # HOUR12
+    '%k',  # HOUR24_PRECEDED_BY_A_SPACE
+    '%l',  # HOUR12_PRECEDED_BY_A_SPACE
+    '%M',  # MINUTE
+    '%S',  # SECOND
+    '%s',  # EPOCH
+    '%f',  # MICROSECOND
 
-    'AM_OR_PM_UPPER': '%p',
-    'AM_OR_PM_LOWER': '%P',
-    'UTC_OFFSET': '%z',
-    'TIME_ZONE_NAME': '%Z',
+    '%p',  # AM_OR_PM_UPPER
+    '%P',  # AM_OR_PM_LOWER
+    '%z',  # UTC_OFFSET
+    '%Z',  # TIME_ZONE_NAME
 
     # format codes
-    'HOUR_MINUTE': '%R',
-    'HOUR_MINUTE_SECOND': '%T',
-    'LOCALE_DATE_TIME': '%c',
-    'LOCALE_DATE': '%x',  # MM/DD/YY
-    'LOCALE_TIME': '%X',  # HH:MM:SS
+    '%R',  # HOUR_MINUTE
+    '%T',  # HOUR_MINUTE_SECOND
 
     # special characters
-    'NEW_LINE': '%n',
-    'TAB': '%t',
-    'PERCENT_CHAR': '%%',
+    '%n',  # NEW_LINE
+    '%t',  # TAB
+    '%%',  # PERCENT_CHAR
 }
 
 # https://www.postgresql.org/docs/current/functions-formatting.html#FUNCTIONS-FORMATTING-DATETIME-TABLE
-_POSTGRES_DATE_FORMAT_CODES = {
-    # week codes
-    'WEEKDAY_ABBREVIATED': 'Dy',
-    'WEEKDAY_FULL_NAME': 'FMDay',
-    'WEEKDAY_NUMBER': 'D',  # Sunday is 1 and Saturday is 7
-
-    # day codes
-    'DAY_OF_MONTH': 'DD',
-    'DAY_OF_MONTH_SUPPRESSED': 'FMDD',
-    'DAY_OF_YEAR': 'DDD',
-    'DAY_OF_YEAR_SUPPRESSED': 'FMDDD',
-
-    # month codes
-    'MONTH_ABBREVIATED': 'Mon',
-    'MONTH_ABBREVIATED_2': 'Mon',
-    'MONTH_FULL_NAME': 'FMMonth',
-    'MONTH_NUMBER': 'MM',
-    'MONTH_NUMBER_SUPPRESSED': 'FMMM',
-
-    # year codes
-    'YEAR_WITHOUT_CENTURY': 'YY',
-    'YEAR_WITH_CENTURY': 'YYYY',
-    'CENTURY': 'CC',
-
-    # quarter
-    'QUARTER': 'Q',
-
-    # date formats
-    'MONTH_DAY_YEAR': 'MM/DD/YY',
-    'YEAR_MONTH_DAY': 'YYYY-MM-DD',
-
-    # iso 8601
-    'ISO_8601_YEAR_WITH_CENTURY': 'IYYY',
-    'ISO_8601_YEAR_WITHOUT_CENTURY': 'IY',
-    'ISO_8601_WEEK_NUMBER_OF_YEAR': 'IW',
-    'ISO_8601_WEEKDAY_NUMBER': 'ID',  # Monday is 1, Sunday is 7
-
-    # time units
-    'HOUR24': 'HH24',
-    'HOUR24_SUPPRESSED': 'FMHH24',
-    'HOUR12': 'HH12',
-    'HOUR12_SUPPRESSED': 'FMHH12',
-    'MINUTE': 'MI',
-    'MINUTE_SUPPRESSED': 'FMMI',
-    'SECOND': 'SS',
-    'SECOND_SUPPRESSED': 'FMSS',
-    'MICROSECOND': 'US',
-
-    'AM_OR_PM_UPPER': 'AM',
-    'AM_OR_PM_LOWER': 'am',
-
-    # utc and timezone
-    'UTC_OFFSET': 'OF',
-    'TIME_ZONE_NAME': 'TZ',
-
-    # time formats
-    'HOUR_MINUTE': 'HH24:MI',
-    'HOUR_MINUTE_SECOND': 'HH24:MI:SS',
-
-    # locale based formats
-    'LOCALE_DATE_TIME': 'Dy Mon  FMDD HH24:MI:SS YYYY',
-    'LOCALE_DATE': 'MM/DD/YY',
-    'LOCALE_TIME': 'HH24:MI:SS',
+_C_STANDARD_CODES_X_POSTGRES_DATE_CODES = {
+    "%a": "Dy",
+    "%A": "FMDay",
+    "%w": "D",  # Sunday is 1 and Saturday is 7
+    "%d": "DD",
+    "%j": "DDD",
+    "%b": "Mon",
+    "%h": "Mon",
+    "%B": "FMMonth",
+    "%m": "MM",
+    "%y": "YY",
+    "%Y": "YYYY",
+    "%C": "CC",
+    "%Q": "Q",
+    "%D": "MM/DD/YY",
+    "%F": "YYYY-MM-DD",
+    "%G": "IYYY",
+    "%g": "IY",
+    "%V": "IW",
+    "%u": "ID",
+    "%H": "HH24",
+    "%I": "HH12",
+    "%M": "MI",
+    "%S": "SS",
+    "%f": "US",
+    "%p": "AM",
+    "%P": "am",
+    "%z": "OF",
+    "%Z": "TZ",
+    "%R": "HH24:MI",
+    "%T": "HH24:MI:SS",
 }
 
 
-# BQ has no code for suppressing leading zeros
-_BIGQUERY_DATE_FORMAT_SUPPRESSED_CODES = {
-    'DAY_OF_MONTH_SUPPRESSED': '%d',
-    'DAY_OF_YEAR_SUPPRESSED': '%j',
-    'MONTH_NUMBER_SUPPRESSED': '%m',
-    'HOUR24_SUPPRESSED': '%H',
-    'HOUR12_SUPPRESSED': '%I',
-    'MINUTE_SUPPRESSED': '%M',
-    'SECOND_SUPPRESSED': '%S',
-}
-
-# https://cloud.google.com/bigquery/docs/reference/standard-sql/format-elements#format_elements_date_time
-_BIGQUERY_DATE_FORMAT_CODES = {
-    **_STANDARD_DATE_FORMAT_CODES,
-    **_BIGQUERY_DATE_FORMAT_SUPPRESSED_CODES,
-}
-
-
-def parse_date_format_str(engine: Engine, date_format: str) -> str:
+def parse_c_standard_code_to_postgres_code(date_format: str) -> str:
     """
-    Parses a date format string from standard codes to its supported codes based on engine.
-    If standard code has no equivalent, it will remain in resultant string.
+    Parses a date format string from standard codes to Postgres date codes.
+    If c-standard code has no equivalent, it will remain in resultant string.
+
+    Steps to follow:
+        date_format = '%Y%m-%Y%m-%Y%m%d-%d'
+
+        Step 1: Get all unique groups of continuous c-codes,
+            groups are processed based on length in order to avoid
+            replacing occurrences of other groups (e.g %Y%m occurs in %Y%m%d, but both are different groups):
+                ['%Y%m%d', '%Y%m', '%d']
+
+        Step 2: For each group
+                                                               '%Y%m%d'             %Y%m'      '%d'
+            1) Get individual codes                       ['%Y', '%m', '%d']   ['%Y', '%m']   ['%d']
+            2) Replace with respective Postgres Code     ['YYYY', 'MM', 'DD'] ['YYYY', 'MM']  ['DD']
+            3) Recreate group (joined by ")                 'YYYY"MM"DD'        'YYYY"MM'      'DD'
+            4) Replace all original group occurrences
+               in original date_format string               'YYYY""MM-YYYY""MM-YYYY""MM""DD-DD'
+               with result from previous step
+
+    .. note:: We use double quotes to force TO_CHAR interpret an empty string as literals, this way
+            continuous date codes yield the correct value as intended
+            For example having '%y%Y':
+              TO_CHAR(cast('2022-01-01' as date), 'YY""YYYY') will generate '222022' (correct)
+              vs.
+              TO_CHAR(cast('2022-01-01' as date), 'YYYYYY') will generate '202222' (incorrect)
+
     """
-    if not is_bigquery(engine) and not is_postgres(engine):
+
+    codes_base_pattern = '|'.join(_SUPPORTED_C_STANDARD_CODES)
+    grouped_codes_matches = re.findall(pattern=rf"(?P<codes>({codes_base_pattern})+)", string=date_format)
+    if not grouped_codes_matches:
         return date_format
 
-    if is_bigquery(engine):
-        codes_to_replace = {
-            code_name: _STANDARD_DATE_FORMAT_CODES[code_name]
-            for code_name in _BIGQUERY_DATE_FORMAT_SUPPRESSED_CODES.keys()
-        }
-        new_codes = _BIGQUERY_DATE_FORMAT_SUPPRESSED_CODES
-    else:
-        codes_to_replace = _STANDARD_DATE_FORMAT_CODES
-        new_codes = _POSTGRES_DATE_FORMAT_CODES
-
-    return _replace_date_codes(
-        codes_to_replace=codes_to_replace,
-        new_codes=new_codes,
-        date_format=date_format
-    )
-
-
-def _replace_date_codes(
-    *,
-    codes_to_replace: Dict[str, str],
-    new_codes: Dict[str, str],
-    date_format: str,
-) -> str:
-    """
-    Helper function that replaces a string's date codes with its equivalent.
-    :param codes_to_replace: Dictionary containing all c-standard codes to be replaced.
-    :param new_codes: Dictionary containing codes used for replacement. Code will be used if key exists in
-        codes_to_replace.
-    :param date_format: string to be replaced
-
-    returns the string obtained by replacing old codes with new codes.
-    """
-    to_repl_codes_tokens = re.findall(pattern='|'.join(codes_to_replace.values()), string=date_format)
+    # regex used in findall has 2 groups, we are interested in the first group
+    # Here we get all unique groups of continuous c-codes, sorted by largest to smaller
+    # this way we avoid replacing nested groups
+    tokenized_c_codes = sorted({tokens for tokens, _ in grouped_codes_matches}, key=len, reverse=True)
+    unsupported_c_codes = set()
+    single_c_code_regex = re.compile(rf'{codes_base_pattern}')
 
     new_date_format = date_format
-    for to_repl_code in to_repl_codes_tokens:
-        code_name = [name for name, code in codes_to_replace.items() if code == to_repl_code][0]
-        if code_name not in new_codes:
-            continue
+    for grouped_c_codes in tokenized_c_codes:
+        # get individual codes from the group
+        codes_to_replace = single_c_code_regex.findall(grouped_c_codes)
+        replaced_codes = []
+        for to_repl_code in codes_to_replace:
+            if to_repl_code not in _C_STANDARD_CODES_X_POSTGRES_DATE_CODES:
+                unsupported_c_codes.add(to_repl_code)
+                replaced_codes.append(to_repl_code)
+                continue
 
-        new_code = new_codes[code_name]
-        new_date_format = re.sub(pattern=rf'{to_repl_code}', repl=new_code, string=new_date_format)
+            # get correspondent postgres code
+            replaced_codes.append(_C_STANDARD_CODES_X_POSTGRES_DATE_CODES[to_repl_code])
+
+        # replace all group occurrences
+        new_date_format = re.sub(
+            pattern=rf'({grouped_c_codes})', repl='""'.join(replaced_codes), string=new_date_format,
+        )
+
+    if unsupported_c_codes:
+        warnings.warn(
+            message=f'There are no equivalent codes for {sorted(unsupported_c_codes)}.',
+            category=UserWarning,
+        )
 
     return new_date_format
