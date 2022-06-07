c61bcddecd25fa2f44c03086344612707b9673a3
Kathia Barahona <katbara03@gmail.com>
2022-06-07 16:04:18 +0200

    convert non c-codes to literals

--- bach/tests/unit/bach/test_series_utils_datetime_formats.py
+++ bach/tests/unit/bach/test_series_utils_datetime_formats.py
@@ -9,31 +9,31 @@ def test_parse_c_standard_code_to_postgres_code(recwarn):
     assert parse_c_standard_code_to_postgres_code('%Y') == 'YYYY'
 
     # non supported c-code in bach
-    assert parse_c_standard_code_to_postgres_code('%c') == '%c'
+    assert parse_c_standard_code_to_postgres_code('%c') == '"%c"'
 
     # c-code supported in bach, not in postgres
     assert parse_c_standard_code_to_postgres_code('%s') == '%s'
-    assert parse_c_standard_code_to_postgres_code('%Y-%%%m-%d') == 'YYYY-%%""MM-DD'
+    assert parse_c_standard_code_to_postgres_code('%Y-%%%m-%d') == 'YYYY"-"%%""MM"-"DD'
 
     # simple case
-    assert parse_c_standard_code_to_postgres_code('%Y-%m-%d') == 'YYYY-MM-DD'
+    assert parse_c_standard_code_to_postgres_code('%Y-%m-%d') == 'YYYY"-"MM"-"DD'
 
     # multiple continuous c-codes
-    assert parse_c_standard_code_to_postgres_code('%Y%m-%d%d') == 'YYYY""MM-DD""DD'
+    assert parse_c_standard_code_to_postgres_code('%Y%m-%d%d') == 'YYYY""MM"-"DD""DD'
 
     # date format with extra tokens in start and end
-    assert parse_c_standard_code_to_postgres_code('abc %Y def%') == 'abc YYYY def%'
+    assert parse_c_standard_code_to_postgres_code('abc %Y def%') == '"abc "YYYY" def%"'
 
     # continuous c-codes in single string
     assert parse_c_standard_code_to_postgres_code('%Y%Y%Y') == 'YYYY""YYYY""YYYY'
 
     # nested groups
     assert parse_c_standard_code_to_postgres_code('%Y%m-%Y%m%d-%Y%m-%m%d-%d') == (
-        'YYYY""MM-YYYY""MM""DD-YYYY""MM-MM""DD-DD'
+        'YYYY""MM"-"YYYY""MM""DD"-"YYYY""MM"-"MM""DD"-"DD'
     )
 
     # regular postgres format
-    assert parse_c_standard_code_to_postgres_code('YYYYMMDD') == 'YYYYMMDD'
+    assert parse_c_standard_code_to_postgres_code('YYYYMMDD') == '"YYYYMMDD"'
 
     assert len(recwarn) == 2
 
