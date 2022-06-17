d563a16c0b51e5a9f11c873da57955797802fea1
sdirosa <surai@objectiv.io>
2022-06-17 10:35:20 +0200

    Some more documentation for RecordedEvents interface

--- tracker/core/developer-tools/src/RecordedEvents.ts
+++ tracker/core/developer-tools/src/RecordedEvents.ts
@@ -18,12 +18,28 @@ export type AnyLocationContextNameAndId = `${LocationContextName}:${string}` | `
 export type AnyGlobalContextName = `${GlobalContextName}`;
 export type AnyGlobalContextNameAndId = `${GlobalContextName}:${string}` | `:${string}`;
 
+/**
+ * RecordedEvents instances can filter the given RecordedEvents by event type and/or context name and id.
+ */
 export class RecordedEvents {
   readonly events: RecordedEvent[];
+
+  /**
+   * RecordedEvents is constructed with a list of RecordedEvents, stored in its internal state for later processing
+   */
   constructor(events: RecordedEvent[]) {
     this.events = events;
   }
 
+  /**
+   * The filter method allows to filter by Event _type. It supports querying by:
+   *
+   * - a single event name, e.g. `PressEvent`
+   * - a list of event names, e.g. [`PressEvent`, `ApplicationLoadedEvent`]
+   * - a predicate, for advanced operations, e.g. (event) => boolean
+   *
+   * `filter` returns a new instance of RecordedEvents for further chaining.
+   */
   filter(name: AnyEventName): RecordedEvents;
   filter(names: AnyEventName[]): RecordedEvents;
   filter(predicate: RecordedEventPredicate): RecordedEvents;
@@ -44,6 +60,15 @@ export class RecordedEvents {
     throw new Error(`Invalid event filter options: ${JSON.stringify(options)}`);
   }
 
+  /**
+   * The withLocationContext method allows to filter LocationContexts by name, name and id or just id. It supports:
+   *
+   * - a Location Context name, e.g. `RootLocationContext`
+   * - a Location Context name and its identifier, separated by a colon, e.g. `RootLocationContext:home`
+   * - a Location Context identifier, prefixed by a colon, e.g. `:home`
+   *
+   * `withLocationContext` returns a new instance of RecordedEvents for further chaining.
+   */
   withLocationContext(nameAndMaybeId: AnyLocationContextName | AnyLocationContextNameAndId) {
     if (typeof nameAndMaybeId !== 'string') {
       throw new Error(`Invalid location context filter options: ${JSON.stringify(nameAndMaybeId)}`);
@@ -54,6 +79,15 @@ export class RecordedEvents {
     return new RecordedEvents(this.events.filter((event) => hasContext(event.location_stack, name, id)));
   }
 
+  /**
+   * The withGlobalContext method allows to filter GlobalContexts by name, name and id or just id. It supports:
+   *
+   * - a Global Context name, e.g. `PathContext`
+   * - a Global Context name and its identifier, separated by a colon, e.g. `PathContext:http://localhost/`
+   * - a Global Context identifier, prefixed by a colon, e.g. `:http://localhost/`
+   *
+   * `withGlobalContext` returns a new instance of RecordedEvents for further chaining.
+   */
   withGlobalContext(nameAndMaybeId: AnyGlobalContextName | AnyGlobalContextNameAndId) {
     if (typeof nameAndMaybeId !== 'string') {
       throw new Error(`Invalid global context filter options: ${JSON.stringify(nameAndMaybeId)}`);
