271ece01a8f5095b15ee86aa74828e4bd3f38a49
Kathia Barahona <katbara03@gmail.com>
2022-06-23 14:03:09 +0200

    run bq for test_top_product_features_before_conversion

--- modelhub/tests_modelhub/functional/modelhub/test_model_hub_top_product_features_before_conversion.py
+++ modelhub/tests_modelhub/functional/modelhub/test_model_hub_top_product_features_before_conversion.py
@@ -9,8 +9,8 @@ from tests_modelhub.data_and_utils.utils import get_objectiv_dataframe_test
 from tests.functional.bach.test_data_and_utils import assert_equals_data
 
 
-def test_top_product_features_before_conversion():
-    df, modelhub = get_objectiv_dataframe_test()
+def test_top_product_features_before_conversion(db_params):
+    df, modelhub = get_objectiv_dataframe_test(db_params)
     initial_columns = df.data_columns
 
     event_type = 'ClickEvent'
