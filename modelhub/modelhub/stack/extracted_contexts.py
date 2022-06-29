f6e3cf77a4a58d676fffb8f82b7a14dd7643c99e
Kathia Barahona <katbara03@gmail.com>
2022-06-29 15:17:05 +0200

    drop duplicated event ids based on collector_tstamp

--- modelhub/modelhub/stack/extracted_contexts.py
+++ modelhub/modelhub/stack/extracted_contexts.py
@@ -91,7 +91,9 @@ class ExtractedContextsPipeline(BaseDataPipeline):
             'moment': bach.SeriesTimestamp.dtype,
             'cookie_id': bach.SeriesUuid.dtype,
         },
-        DBDialect.BIGQUERY: {},
+        DBDialect.BIGQUERY: {
+            'collector_tstamp': bach.SeriesTimestamp.dtype,
+        },
     }
 
     def __init__(self, engine: Engine, table_name: str):
@@ -218,7 +220,7 @@ class ExtractedContextsPipeline(BaseDataPipeline):
         # In all cases we are only interested in the first event. On postgres we achieve this by having a
         # primary key index on event-id. On BigQuery such indexes are not possible. Instead, we here filter
         # out duplicate event-ids, keeping the first event with each id.
-        df_cp = df_cp.drop_duplicates(subset=['event_id'], sort_by=['time'], keep='first')
+        df_cp = df_cp.drop_duplicates(subset=['event_id'], sort_by=['collector_tstamp'], keep='first')
 
         # BQ data source has no moment and day columns, therefore we need to generate them
         # based on the time value from the taxonomy column
