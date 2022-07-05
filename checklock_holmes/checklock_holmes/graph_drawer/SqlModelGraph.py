bed8cc38ca8a71e8f6d90f0263dfba82dd0845c5
Kathia Barahona <katbara03@gmail.com>
2022-07-05 11:22:47 +0200

    graphin nodes notebook

--- checklock_holmes/checklock_holmes/graph_drawer/SqlModelGraph.py
+++ checklock_holmes/checklock_holmes/graph_drawer/SqlModelGraph.py
@@ -1,10 +1,8 @@
 from copy import deepcopy
 from functools import cached_property
 
-import graph_tool as gt
-from bach.merge import MergeSqlModel
-from graph_tool.draw import graph_draw, planar_layout
 from sql_models.model import SqlModel
+from graphviz import Digraph
 
 
 class SqlModelGraph:
@@ -14,26 +12,14 @@ class SqlModelGraph:
 
     def __init__(self, node: SqlModel):
         self._last_node = node
-        self._node_name_x_vertex_id = {}
+        self._added_nodes = []
         self._initialize_graph()
 
     def draw(self) -> None:
-        pos = planar_layout(self._graph)
-        graph_draw(
-            self._graph,
-            pos=pos,
-            vertex_text=self._graph.vp[self.NODE_NAME_PROP_NAME],
-            #edge_text=self._graph.ep[self.NODE_IS_MERGE_PROP_NAME],
-            vprops={
-                'size': 10,
-                'text_position': 'centered'
-            },
-            output_size=(1000, 1000),
-            output="sql_model_graph.pdf",
-        )
+        self._graph.render('doctest-output/round-table.gv').replace('\\', '/')
 
     @property
-    def graph(self) -> gt.Graph:
+    def graph(self) -> Digraph:
         return self._graph
 
     @cached_property
@@ -45,49 +31,21 @@ class SqlModelGraph:
         return current_node
 
     def _initialize_graph(self) -> None:
-        self._graph = gt.Graph()
-        self._graph.vp[self.NODE_NAME_PROP_NAME] = self._graph.new_vp('string')
-        self._graph.vp[self.NODE_ORDER_PROP_NAME] = self._graph.new_vp('int')
-        self._graph.ep[self.NODE_IS_MERGE_PROP_NAME] = self._graph.new_ep('string')
-
-        # add first node
-        ln_vertex = self._add_node_as_vertex(self._last_node)
-        self._add_node_references(self._last_node, ln_vertex)
-
-        self._assign_order_creation_property()
+        self._graph = Digraph()
+        self._add_node_references(self._last_node)
 
     def _add_node_references(
-        self, current_node: SqlModel, current_vertex_id: int,
+        self, current_node: SqlModel,
     ) -> None:
+        self._add_node_as_vertex(current_node)
         for ref in current_node.references.values():
-            ref_vertex_id = self._add_node_as_vertex(ref)
-            # don't add the edge if it already exist
-            if self._graph.edge(ref_vertex_id, current_vertex_id):
-                continue
+            self._graph.edge(ref.hash, current_node.hash)
 
-            e_id = self._graph.add_edge(source=ref_vertex_id, target=current_vertex_id)
-            if isinstance(current_node, MergeSqlModel):
-                self._graph.ep[self.NODE_IS_MERGE_PROP_NAME][e_id] = 'merge'
+            if ref.hash not in self._added_nodes:
+                self._add_node_references(ref)
 
-            if ref.references:
-                self._add_node_references(
-                    current_node=ref, current_vertex_id=ref_vertex_id,
-                )
-
-    def _add_node_as_vertex(self, node: SqlModel) -> int:
-
-        node_name = f'{node.generic_name}_{node.hash}'
-        if node_name in self._node_name_x_vertex_id:
-            return self._node_name_x_vertex_id[node_name]
-
-        vertex_id = self._graph.add_vertex()
-        self._node_name_x_vertex_id[node_name] = vertex_id
-
-        short_node_name = f'{node.generic_name}_{node.hash[0:3]}...{node.hash[-3:]}'
-        self._graph.vp[self.NODE_NAME_PROP_NAME][vertex_id] = short_node_name
-
-        return vertex_id
-
-    def _assign_order_creation_property(self) -> None:
-        for creation_index, v_index in enumerate(reversed(self._graph.get_vertices())):
-            self._graph.vp[self.NODE_ORDER_PROP_NAME][v_index] = creation_index
+    def _add_node_as_vertex(self, node: SqlModel) -> None:
+        if node.hash in self._added_nodes:
+            return
+        self._graph.node(name=node.hash, label=f'{node.generic_name}_{node.hash}')
+        self._added_nodes.append(node.hash)
