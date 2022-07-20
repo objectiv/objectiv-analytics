47b858a6e777e33e7a2671169fbafc1e303ecbde
Tom Jansen <10804239+jansentom@users.noreply.github.com>
2022-07-20 13:16:20 +0200

    Revert "skip tests using pg_engine when running for bq only"
    This reverts commit 78f2d8407413312200df524845470b6baebb2ac7.

--- bach/tests/conftest.py
+++ bach/tests/conftest.py
@@ -76,11 +76,7 @@ _RECORDED_TEST_TABLES_PER_ENGINE: [Engine, List[str]] = defaultdict(list)
 
 
 @pytest.fixture()
-def pg_engine(request: SubRequest) -> Engine:
-    if request.config.getoption("big_query"):
-        # Skip tests using this fixture when running only for big_query
-        pytest.skip()
-
+def pg_engine() -> Engine:
     # TODO: port all tests that use this to be multi-database. Or explicitly mark them as skip-bigquery
     return _ENGINE_CACHE[DB.POSTGRES]
 
